ef721323f6b388f228abace75febe1afe1f74c26
Hugh <hugh_gleaves@hotmail.com>
2025-05-07 10:17:10 -0700

    Play with talking to an NRF24L01+

---
+++ samples/Example.CommandLine/CONFIG.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example.CommandLine
+{
+    public struct CONFIG
+    {
+        public byte register;
+
+        public ref byte[] Buffer
+        { get => ref new byte[] { register };
+          set 
+            { 
+              register = value[0]; 
+            }
+        }
+
+        public bool RESERVED
+        {
+            get
+            {
+                return (register & 0x80) != 0;
+            }   
+        }
+
+        public bool MASK_RX_DR
+        {
+            get
+            {
+                return (register & 0x40) != 0;
+            }
+        }
+
+        public bool MASK_TX_DS
+        {
+            get
+            {
+                return (register & 0x20) != 0;
+            }
+        }
+
+        public bool MASK_MAX_RT
+        {
+            get
+            {
+                return (register & 0x10) != 0;
+            }
+        }
+
+        public bool MASK_EN_CRC
+        {
+            get
+            {
+                return (register & 0x08) != 0;
+            }
+        }
+
+        public bool CRCO
+        {
+            get
+            {
+                return (register & 0x04) != 0;
+            }
+        }
+
+        public bool PWR_UP
+        {
+            get
+            {
+                return (register & 0x02) != 0;
+            }
+        }
+
+        public bool PRIM_RX
+        {
+            get
+            {
+                return (register & 0x01) != 0;
+            }
+        }
+    }
+}
