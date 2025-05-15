c3709dbb9f2f2e0f32f674b45c1b874a08957e3d
Hugh <hugh_gleaves@hotmail.com>
2025-05-15 11:43:49 -0700

    Correct the STATUS class.

--- Futurist.Nordic.NRF244L01P/Registers/STATUS.cs
+++ Futurist.Nordic.NRF244L01P/Registers/STATUS.cs
@@ -6,9 +6,21 @@
         {
             Id = 0x07;
         }
-        public bool RX_DR => BIT6;
-        public bool TX_DS => BIT5;
-        public bool MAX_RT => BIT4;
+        public bool RX_DR
+        {
+            get => BIT6;
+            set => BIT6 = value;
+        }
+        public bool TX_DS
+        {
+            get => BIT5;
+            set => BIT5 = value;
+        }
+        public bool MAX_RT
+        {
+            get => BIT4;
+            set => BIT4 = value;
+        }
         public byte RX_P_NO => (byte)((Register[0] & 0x0E) >> 1);
         public bool TX_FULL => BIT0;
     }
